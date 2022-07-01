d3c5c0456d8a19e01463bb0f0e902bc05c4f7c77
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-07-02 02:00:02 +0530

    refactored : Ensure win at 100 only not more nor less

--- src/com/snakeandladders/SnakeAndLadderSimulator.java
+++ src/com/snakeandladders/SnakeAndLadderSimulator.java
@@ -16,42 +16,50 @@ public class SnakeAndLadderSimulator {
 			System.out.println("    Opening chance : " + opening);
 		}
 		if (opening == 1) {
-			while (visited < end) {
+			while (visited != end) {
 				rolldice = (int) (1 + Math.floor(Math.random() * 10) % 6); // roll the the dice to get number
-				visited = visited + rolldice; // add rolldice to get new position
+				visited = visited + rolldice;
+				if (visited < 100) {
 
-				// Ladders starts from here
-				if (visited == 2)
-					visited = 23;
-				else if (visited == 6)
-					visited = 28;
-				else if (visited == 32)
-					visited = 51;
-				else if (visited == 41)
-					visited = 61;
-				else if (visited == 65)
-					visited = 87;
-				else if (visited == 89)
-					visited = 95;
-				else if (visited == 15)
-					visited = 4;
-				// snakes start from here
-				else if (visited == 21)
-					visited = 7;
-				else if (visited == 56)
-					visited = 13;
-				else if (visited == 49)
-					visited = 19;
-				else if (visited == 63)
-					visited = 44;
-				else if (visited == 85)
-					visited = 38;
-				else if (visited == 31)
-					visited = 12;
-				else if (visited == 99)
-					visited = 35;
+					// Ladders starts from here
+					if (visited == 2)
+						visited = 23;
+					else if (visited == 6)
+						visited = 28;
+					else if (visited == 32)
+						visited = 51;
+					else if (visited == 41)
+						visited = 61;
+					else if (visited == 65)
+						visited = 87;
+					else if (visited == 89)
+						visited = 95;
+					else if (visited == 15)
+						visited = 4;
+					// snakes start from here
+					else if (visited == 21)
+						visited = 7;
+					else if (visited == 56)
+						visited = 13;
+					else if (visited == 49)
+						visited = 19;
+					else if (visited == 63)
+						visited = 44;
+					else if (visited == 85)
+						visited = 38;
+					else if (visited == 31)
+						visited = 12;
+					else if (visited == 99)
+						visited = 35;
 
-				System.out.println("\n     Current position is  : " + visited + " ,     Rolldice value : " + rolldice);
+					System.out.println("     Current position is  : " + visited + " ,  -   Rolldice value : " + rolldice);
+				} else if (visited == end) {
+					System.out.println("     Current position is  : " + visited + " ,     Rolldice value : " + rolldice);
+				} else if (visited > end) {
+					visited=visited-rolldice;
+					System.out.println("     Skip Step");
+				}
+
 			}
 		} else {
 			visited = start;
